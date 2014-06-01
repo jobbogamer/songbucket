7ba767bb7bbfc4c123b8b1ada58d21a88e6a2ad6
Josh Asch <josh.asch@gmail.com>
2014-06-01 14:53:31 +0100

    Basic html structure

--- tunebucket.py
+++ tunebucket.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, flash, url_for, request, redirect, jsonify
 
 app = Flask(__name__)
 
@@ -7,8 +7,19 @@ app = Flask(__name__)
 ###############################################################################
 
 @app.route('/')
-def index():
-	return "Hello, world!"
+def home():
+	options = { }
+	return render_template('index.html', options=options)
+
+@app.route('/favourites')
+def favourites():
+	options = {	'title': "Favourites" }
+	return render_template('favourites.html', options=options)
+
+@app.route('/add')
+def add_song():
+	options = { 'title': "Add Song" }
+	return render_template('add.html', options=options)
 
 ###############################################################################
 # "Main"                                                                      #
